e7f69dd70e5307498bc09ecf566f5e8c1a4fade2
Ankita Sivaramakrishnan <154521450+thecerebralcosmocrat@users.noreply.github.com>
2025-09-05 18:37:01 +0530

    Time: 12 ms (36.56%) | Memory: 49.5 MB (86.78%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,8 +1,23 @@
-// brute force (sorting) approach
+// two-pass HashMap/counting method
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int majority = nums[nums.length/2];
+        int majority = 0;
+        HashMap<Integer, Integer> freqMapM = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            freqMapM.put(nums[i], freqMapM.getOrDefault(nums[i], 0) +1);
+        }
+        for(Integer key : freqMapM.keySet()){
+        // the keySet() method returns a set of all keys in the HashMap
+        // you iterate through each key in the HashMap
+            if(freqMapM.get(key) > nums.length/2){
+            //freqMapM.get(key) retrieves the value associated with the key
+            // the value being the frequency of occurence of the element
+            // checks if the frequency of occurence is greater than nums.length/2
+                majority = key;
+                // if yes assign key (the element) to majority
+            }
+        }
         return majority;
+        // return majority (every array will definitely have a majority element)
     }
 }
