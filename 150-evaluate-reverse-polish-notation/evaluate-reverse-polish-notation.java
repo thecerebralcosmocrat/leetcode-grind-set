c15d8900ca95bb0c36bde8593dce6c96723ffe1b
Ankita Sivaramakrishnan <154521450+thecerebralcosmocrat@users.noreply.github.com>
2025-09-23 22:33:47 +0530

    Time: 5 ms (97.30%) | Memory: 44.9 MB (31.34%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,17 +1,16 @@
 class Solution {
     public int evalRPN(String[] tokens) {
     
-    // RPN = regular postfix expression 
-    // the operator working on two specific operands immediately succeeds them
-    // (3 + 4 - 5) is evaluated as (3 4 + 5 -) in RPN/postfix form
-    
-    Stack<Integer> stack = new Stack<>();
-    // we push elements as Integer into stack
+    Deque<Integer> stack = new ArrayDeque<>();
+    // using ArrayDeque under the hood to implement Stack
+    // faster because its unsynchronized
 
-    for(int i=0; i<tokens.length; i++){
-        String token = tokens[i];
+    Set<String> ops = Set.of("+", "-", "*", "/");
+    // Set of valid operators
 
-        if(token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")){
+    for(String token : tokens){
+
+        if(ops.contains(token)){
             int b = stack.pop();
             int a = stack.pop();
             int result = 0;
@@ -21,20 +20,14 @@ class Solution {
                 case "-": result = a - b; break;
                 case "*": result = a * b; break;
                 case "/": result = a / b; break; 
-                // integer division in Java truncates to zero by default
-                // + and * are commutative but - and / are not
-                // - and / are cases that will break code if elements are not popped properly
+                // integer division truncates towards zero by default
             }
             stack.push(result);
         } else {
             stack.push(Integer.parseInt(token));
-            // Integer.parseInt(token) returns a primitive int which is autoboxed to Integer when pushed
-            // stack.pop() returns an Integer which is auto-unboxed to int when used in arithmetic or assigned to an int
-            // Be careful: unboxing a null Integer will throw NullPointerException
         }
     }
     return stack.pop();
-    // return the final element pushed into the stack by popping it
-    // this element will be the final answer to the evaluated RPN/postfix expression
+    // final result
   }
 }
